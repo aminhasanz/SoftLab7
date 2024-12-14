4c5af4ab4f30509a506a00116c1c25e45d04fb62
smmsajjadi <93717646+smmsajjadi@users.noreply.github.com>
2024-12-14 22:07:33 +0330

    State/Strategy with add DijkstraTraversal and BFSTraversal and GraphTraversalStrategy and edit this class

--- java/graph/Graph.java
+++ java/graph/Graph.java
@@ -1,65 +1,14 @@
-package graph;
-
-import lombok.Getter;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.stream.Collectors;
-
-public class Graph {
+class Graph {
     @Getter
-    private ArrayList<Node> graph;
+    private final ArrayList<Node> graph;
+    private GraphTraversalStrategy traversalStrategy;
 
-    public Graph(ArrayList<Node> graph) {
-        this.graph = graph;
+    public Graph() {
+        this.graph = new ArrayList<>();
     }
 
-    public void resetVisits() {
-        for (Node v : this.getGraph())
-            v.setVisited(false);
-    }
-
-    public void bfs(Node s) {
-        this.resetVisits();
-
-        Queue<Pair<Node, Integer>> nodes = new LinkedList<>();
-        nodes.add(new Pair<Node, Integer>(s, 0));
-        while (!nodes.isEmpty()) {
-            Pair<Node, Integer> front = nodes.poll();
-            Node frontNode = front.getValue0();
-            if (!frontNode.isVisited()) {
-                frontNode.setVisited(true);
-                int distance = front.getValue1();
-                frontNode.setDistance(distance);
-                nodes.addAll(frontNode.getAvailableNeighbors()
-                        .stream()
-                        .map(neighbor -> new Pair<Node, Integer>(neighbor, distance + 1))
-                        .collect(Collectors.toCollection(ArrayList::new)));
-            }
-        }
-    }
-
-    public void dijkstra(Node s) {
-        this.resetVisits();
-
-        PriorityQueue<Pair<Integer, Node>> nodes = new PriorityQueue<>();
-        nodes.add(new Pair<Integer, Node>(0, s));
-        while (!nodes.isEmpty()) {
-            Pair<Integer, Node> front = nodes.poll();
-            Node frontNode = front.getValue1();
-            if (!frontNode.isVisited()) {
-                frontNode.setVisited(true);
-                int distance = front.getValue0();
-                frontNode.setDistance(distance);
-                nodes.addAll(frontNode.getAvailableWeightedNeighbors()
-                        .stream()
-                        .map(neighbor -> new Pair<Integer, Node>(neighbor.getValue1() + distance,
-                                neighbor.getValue0()))
-                        .collect(Collectors.toCollection(PriorityQueue::new)));
-            }
-        }
+    public void setTraversalStrategy(GraphTraversalStrategy strategy) {
+        this.traversalStrategy = strategy;
     }
 
     public void traverse(Node start) {
@@ -67,6 +16,12 @@ public class Graph {
             traversalStrategy.traverse(this, start);
         } else {
             throw new IllegalStateException("Traversal strategy not set.");
+        }
+    }
+
+    public void resetVisits() {
+        for (Node node : graph) {
+            node.setVisited(false);
         }
     }
 
@@ -79,5 +34,4 @@ public class Graph {
     public Edge addEdge(Node a, Node b, boolean directed, int weight) {
         return Edge.createEdge(a, b, directed, weight);
     }
-
 }
